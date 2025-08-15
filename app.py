bbc183c7230a51d549910bc001e0a02dd9a4e3e9
Aamir Imam <114291815+aamir-10@users.noreply.github.com>
2025-08-15 19:31:26 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,19 +1,18 @@
 from flask import Flask, render_template
 import plotly.io as pio
-from charts import generate_charts  
+from charts import generate_charts 
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    charts_data = generate_charts()
+    charts_data= generate_charts()
     chart_html_list = [
         (pio.to_html(fig, full_html=False, include_plotlyjs='cdn'), title)
-        for fig, title in charts_data
+        for fig,title in charts_data
     ]
-    return render_template('index.html', charts=chart_html_list)
+    return render_template('index.html',charts=chart_html_list)
 
 
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0', port=5000)
-
+    app.run(debug=True,host='0.0.0.0',port=5000)
